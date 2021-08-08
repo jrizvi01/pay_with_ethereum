b7cbdf6bbb1c1fe3bbf6dfe192e634b0e142da66
Jamal Rizvi <80922524+jrizvi01@users.noreply.github.com>
2021-08-07 20:21:04 -0700

    Final Version 08/07/2021 08:20 PM PDT

--- fintech_finder.py
+++ fintech_finder.py
@@ -1,30 +1,6 @@
 # Cryptocurrency Wallet
 
 ################################################################################
-# For this Challenge, you will assume the perspective of a Fintech Finder
-# customer in order to do the following:
-
-# * Generate a new Ethereum account instance by using your mnemonic seed phrase
-# (which you created earlier in the module).
-
-# * Fetch and display the account balance associated with your Ethereum account
-# address.
-
-# * Calculate the total value of an Ethereum transaction, including the gas
-# estimate, that pays a Fintech Finder candidate for their work.
-
-# * Digitally sign a transaction that pays a Fintech Finder candidate, and send
-# this transaction to the Kovan testnet.
-
-# * Review the transaction hash code associated with the validated blockchain transaction.
-
-# Once you receive the transaction’s hash code, you will navigate to [Kovan’s
-# Etherscan](https://kovan.etherscan.io/) website to review the blockchain
-# transaction details. To confirm that you have successfully created the
-# transaction, you will save screenshots to the README.md file of your GitHub
-# repository for this Challenge assignment.
-
-################################################################################
 # Imports
 import streamlit as st
 from dataclasses import dataclass
